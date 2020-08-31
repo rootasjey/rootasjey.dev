c8c147a907e80d87f8dbc26984dc410d8377f01a
rootasjey <jeremiecorpinot@outlook.com>
2020-08-31 14:07:21 +0200

    ➕ Add props. to PostHeadline

--- lib/types/post_headline.dart
+++ lib/types/post_headline.dart
@@ -1,22 +1,45 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rootasjey/types/urls.dart';
 
 class PostHeadline {
   final String id;
   final String title;
+  final String summary;
+  final String timeToRead;
   final DateTime createdAt;
   final DateTime updatedAt;
+  final List<String> tags;
+  Urls urls;
 
   PostHeadline({
     this.id,
     this.title,
+    this.summary,
+    this.timeToRead,
+    this.tags = const [],
     this.createdAt,
     this.updatedAt,
+    this.urls,
   });
 
   factory PostHeadline.fromJSON(Map<String, dynamic> data) {
+    final _tags = <String>[];
+
+    if (data['tags'] != null) {
+      Map<String, dynamic> mapTags = data['tags'];
+
+      mapTags.forEach((key, value) {
+        _tags.add(key);
+      });
+    }
+
     return PostHeadline(
       id: data['id'],
       title: data['title'],
+      timeToRead: data['timeToRead'],
+      summary: data['summary'],
+      tags: _tags,
+      urls: Urls.fromJSON(data['urls']),
       createdAt : (data['createdAt'] as Timestamp).toDate(),
       updatedAt : (data['updatedAt'] as Timestamp).toDate(),
     );
