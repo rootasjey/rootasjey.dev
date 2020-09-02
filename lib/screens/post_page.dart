907122cdbc9e0108d7b8047fe73a1c2423567817
rootasjey <jeremiecorpinot@outlook.com>
2020-09-02 16:41:05 +0200

    💉Replace markdown_widget -> markdown+flutter_html

--- lib/screens/post_page.dart
+++ lib/screens/post_page.dart
@@ -2,12 +2,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:markdown_widget/config/widget_config.dart';
-import 'package:markdown_widget/markdown_widget.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_html/style.dart';
+import 'package:markdown/markdown.dart' as markdown;
 import 'package:rootasjey/components/home_app_bar.dart';
 import 'package:rootasjey/state/colors.dart';
 import 'package:rootasjey/types/post.dart';
 import 'package:rootasjey/utils/snack.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:supercharged/supercharged.dart';
 
 class PostPage extends StatefulWidget {
   @required final String id;
@@ -21,11 +24,12 @@ class PostPage extends StatefulWidget {
 class _PostPageState extends State<PostPage> {
   String postData = '';
   bool isLoading = false;
-  final postScroller = ScrollController();
-  final tocController = TocController();
+  final scrollController = ScrollController();
   bool isTOCVisible = false;
   Post post;
   bool isFabVisible = false;
+
+  final textWidth = 630.0;
 
   @override
   initState() {
@@ -41,7 +45,11 @@ class _PostPageState extends State<PostPage> {
         ? FloatingActionButton(
           backgroundColor: stateColors.primary,
           foregroundColor: Colors.white,
-          onPressed: () => tocController.jumpTo(index: 0),
+          onPressed: () => scrollController.animateTo(
+            0,
+            duration: 250.milliseconds,
+            curve: Curves.bounceOut,
+          ),
           child: Icon(Icons.arrow_upward),
         )
         : Padding(padding: EdgeInsets.zero),
@@ -58,6 +66,7 @@ class _PostPageState extends State<PostPage> {
           return false;
         },
         child: CustomScrollView(
+          controller: scrollController,
           slivers: [
             HomeAppBar(
               automaticallyImplyLeading: true,
@@ -77,106 +86,47 @@ class _PostPageState extends State<PostPage> {
 
   Widget body() {
     if (isLoading) {
-      return SliverList(
-        delegate: SliverChildListDelegate([
-          Center(
-            child: Text('Loading'),
-          ),
-        ]),
-      );
+      return loadingView();
     }
 
     return SliverList(
       delegate: SliverChildListDelegate([
-        SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: Stack(
+        Row(
+          children: [
+            Spacer(),
+            markdownViewer(),
+            Spacer(),
+          ],
+        ),
+      ]),
+    );
+  }
+
+  Widget loadingView() {
+    return SliverList(
+      delegate: SliverChildListDelegate.fixed([
+        Padding(
+          padding: const EdgeInsets.only(top: 200.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              MarkdownWidget(
-                controller: tocController,
-                childMargin: const EdgeInsets.only(
-                  top: 40.0,
-                ),
-                widgetConfig: WidgetConfig(
-                  p: (node) {
-                    return Column(
-                      children: node.children.map((childNode) {
-                        if (childNode.textContent.contains("<img")) {
-                          return imgElement(childNode.textContent);
-                        }
+              CircularProgressIndicator(),
 
-                        if (childNode.textContent.contains("<spacing")) {
-                          return spacingElement(childNode.textContent);
-                        }
-
-                        return SizedBox(
-                          width: 630.0,
-                          child: Text(
-                            childNode.textContent,
-                            textAlign: TextAlign.start,
-                            style: TextStyle(
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.w200,
-                              height: 1.5,
-                            ),
-                          ),
-                        );
-                      }).toList(),
-                    );
-                  },
-                ),
-                styleConfig: StyleConfig(
-                  titleConfig: TitleConfig(
-                    h1: TextStyle(
-                      fontSize: 60.0,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    h2: TextStyle(
+              Opacity(
+                opacity: 0.6,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 40.0,
+                  ),
+                  child: Text(
+                    'Loading...',
+                    style: TextStyle(
                       fontSize: 40.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    h3: TextStyle(
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w400,
-                    ),
-                    titleWrapper: (child) {
-                      return Center(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 630.0,
-                              child: child,
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                    showDivider: false,
-                  ),
-                  ulConfig: UlConfig(
-                    textStyle: TextStyle(
-                      fontSize: 22.0,
                       fontWeight: FontWeight.w300,
-                      height: 1.6,
-                      // height: 1.5,
-                    ),
-                    ulWrapper: (child) {
-                      return SizedBox(
-                        width: 630.0,
-                        child: child,
-                      );
-                    },
-                    dotMargin: const EdgeInsets.only(
-                      top: 13.0,
-                      right: 20.0,
                     ),
                   ),
                 ),
-                data: postData,
               ),
-
-              tocElement(),
             ]
           ),
         ),
@@ -184,68 +134,98 @@ class _PostPageState extends State<PostPage> {
     );
   }
 
-  Widget tocElement() {
-    if (!isTOCVisible) {
-      return Padding(padding: EdgeInsets.zero,);
-    }
-
-    return Positioned(
-      right: 0.0,
-      child: Container(
-        height: MediaQuery.of(context).size.height,
-        width: 260.0,
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20.0,
-          vertical: 10.0,
-        ),
-        child: TocListWidget(
-          controller: tocController,
-          // tocItem: (toc, isSelected) {
-          //   return InkWell(
-          //     onTap: () {},
-          //     child: Padding(
-          //       padding: const EdgeInsets.symmetric(
-          //         vertical: 8.0,
-          //         horizontal: 8.0,
-          //       ),
-          //       child: Text(toc.name),
-          //     ),
-          //   );
-          // },
+  Widget markdownViewer() {
+    return Expanded(
+      flex: 2,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 400.0),
+        child: Html(
+          data: postData,
+          customRender: {
+            'a': (context, child, attributes, element) {
+              return textLink(
+                href: attributes['href'],
+                child: child,
+              );
+            },
+            'img': (context, child, attributes, element) {
+              return imageViewer(
+                context: context.buildContext,
+                src: attributes['src'],
+                alt: attributes['alt'],
+                width: double.parse(attributes['width'], (value) => 300.0),
+                height: double.parse(attributes['height'], (value) => 300.0),
+              );
+            }
+          },
+          style: {
+            'p': Style(
+              width: textWidth,
+              fontSize: FontSize(24.0),
+              fontWeight: FontWeight.w200,
+              lineHeight: 1.5,
+              margin: EdgeInsets.only(
+                top: 40.0,
+                bottom: 20.0,
+              ),
+            ),
+            'ul': Style(
+              fontSize: FontSize(22.0),
+              fontWeight: FontWeight.w300,
+              lineHeight: 1.6,
+            ),
+            'img': Style(
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(
+                top: 40.0,
+                bottom: 20.0,
+              ),
+            ),
+            'h1': Style(
+              width: textWidth,
+              fontSize: FontSize(60.0),
+              fontWeight: FontWeight.w600,
+              margin: EdgeInsets.only(
+                top: 100.0,
+                bottom: 60.0,
+              ),
+            ),
+            'h2': Style(
+              width: textWidth,
+              fontSize: FontSize(50.0),
+              fontWeight: FontWeight.w500,
+              margin: EdgeInsets.only(
+                top: 80.0,
+                bottom: 40.0,
+              ),
+            ),
+            'h3': Style(
+              fontSize: FontSize(30.0),
+              fontWeight: FontWeight.w400,
+            ),
+          },
         ),
       ),
     );
   }
 
-  Widget imgElement(String content) {
-    final imgStartIndex = content.indexOf('src="') + 5;
-    final imgEndIndex = content.indexOf('"', imgStartIndex);
-    final src = content.substring(imgStartIndex, imgEndIndex);
-
-    final altStartIndex = content.indexOf('alt="') + 5;
-    final altEndIndex = content.indexOf('"', altStartIndex);
-    final alt = content.substring(altStartIndex, altEndIndex);
-
-    final widthStartIndex = content.indexOf('width="') + 7;
-    final widthEndIndex = content.indexOf('"', widthStartIndex);
-    final width = content.substring(widthStartIndex, widthEndIndex);
-
-    final heightStartIndex = content.indexOf('height="') + 8;
-    final heightEndIndex = content.indexOf('"', heightStartIndex);
-    final height = content.substring(heightStartIndex, heightEndIndex);
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 60.0,
-      ),
-      child: Column(
-        children: [
-          Card(
-            elevation: 4.0,
+  Widget imageViewer({
+    @required BuildContext context,
+    @required String src,
+    @required String alt,
+    @required double width,
+    @required double height,
+  }) {
+    return Column(
+      children: [
+        Card(
+          elevation: 4.0,
+          child: SizedBox(
+            height: 300.0,
             child: Ink.image(
               image: NetworkImage(src),
-              width: double.parse(width, (value) => 300.0),
-              height: double.parse(height, (value) => 300.0),
+              width: width,
+              height: height,
               fit: BoxFit.cover,
               child: InkWell(
                 onTap: () {
@@ -261,37 +241,45 @@ class _PostPageState extends State<PostPage> {
                   );
                 },
               ),
-            ),
+            )
           ),
+        ),
 
+        if (alt != null && alt.isNotEmpty)
           Padding(
             padding: const EdgeInsets.only(
               top: 8.0,
             ),
-            child: Opacity(
-              opacity: 0.6,
-              child: Text(
-                alt,
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w400,
+            child: FlatButton(
+              onPressed: () => launch(src),
+              child: Opacity(
+                opacity: 0.6,
+                child: Text(
+                  alt,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
               ),
             ),
-          )
-        ],
-      ),
+          ),
+      ],
     );
   }
 
-  Widget spacingElement(String content) {
-    final heightStartIndex = content.indexOf('height="') + 8;
-    final heightEndIndex = content.indexOf('"', heightStartIndex);
-    final height = content.substring(heightStartIndex, heightEndIndex);
-
+  Widget textLink({@required String href, @required Widget child}) {
     return Padding(
-      padding: EdgeInsets.only(
-        top: double.parse(height, (value) => 0.0),
+      padding: const EdgeInsets.only(
+        left: 2.0,
+        right: 2.0,
+        bottom: 5.0,
+      ),
+      child: InkWell(
+        onTap: href != null && href.isNotEmpty
+          ? () => launch(href)
+          : null,
+        child: child,
       ),
     );
   }
@@ -331,7 +319,10 @@ class _PostPageState extends State<PostPage> {
       );
 
       final response = await callable.call({'postId': widget.id});
-      postData = response.data['post'];
+      final markdownData = response.data['post'];
+
+      postData = markdown.markdownToHtml(markdownData);
+      // print(postData);
 
       setState(() => isLoading = false);
 
