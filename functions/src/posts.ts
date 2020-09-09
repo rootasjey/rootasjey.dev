282907c5fcc18acb8e49b39d62aa21352f662bdd
rootasjey <jeremiecorpinot@outlook.com>
2020-09-09 18:00:52 +0200

    👆 (func) Factorize access control

--- functions/src/posts.ts
+++ functions/src/posts.ts
@@ -47,53 +47,7 @@ export const fetch = functions
         'one (string) argument "postId" which is the post to fetch.');
     }
 
-    try { // Access Control
-      const postSnapshot = await firestore
-        .collection('posts')
-        .doc(postId)
-        .get();
-
-      if (!postSnapshot.exists) {
-        throw new functions.https.HttpsError('not-found', 'The post asked does not exist anymore.' +
-          ' You may be asking a deleted post.');
-      }
-
-      const postData = postSnapshot.data();
-
-      if (!postData) {
-        throw new functions.https.HttpsError('data-loss', 'The post data is null, which is weird.' +
-          ' Please contact us <3.');
-      }
-
-      if (!postData['published']) {
-        if (!jwt) {
-          throw new functions.https.HttpsError('unauthenticated', 'The post asked is a draft' +
-            ' and you do not have the right to get its content.');
-        }
-
-        const decodedToken = await auth.verifyIdToken(jwt, true);
-
-        let hasAuthorAccess = false;
-
-        if (postData['author'] === decodedToken.uid) {
-          hasAuthorAccess = true;
-
-        } else if (postData['coauthors'].indexOf(decodedToken.uid) > -1) {
-          hasAuthorAccess = true;
-        }
-
-        if (!hasAuthorAccess) {
-          throw new functions.https.HttpsError('permission-denied', 'You do not have the right' +
-          " to view this post's content.");
-        }
-      } else if (postData['restrictedTo'].premium) {
-        // TODO: Handle premium users.
-      }
-
-    } catch (error) {
-      throw new functions.https.HttpsError('internal', 'There was an internal error' +
-        ' while retrieving the post content. Your JWT may be outdated.');
-    }
+    await checkAccessControl({postId, jwt});
 
     // Retrieve content (access control OK)
     const postFile = storage
@@ -113,6 +67,56 @@ export const fetch = functions
 
     return { post: postContent };
   });
+
+async function checkAccessControl({postId, jwt}: {postId: string, jwt: string}) {
+  try { // Access Control
+    const postSnapshot = await firestore
+      .collection('posts')
+      .doc(postId)
+      .get();
+
+    if (!postSnapshot.exists) {
+      throw new functions.https.HttpsError('not-found', 'The post asked does not exist anymore.' +
+        ' You may be asking a deleted post.');
+    }
+
+    const postData = postSnapshot.data();
+
+    if (!postData) {
+      throw new functions.https.HttpsError('data-loss', 'The post data is null, which is weird.' +
+        ' Please contact us <3.');
+    }
+
+    if (!postData['published']) {
+      if (!jwt) {
+        throw new functions.https.HttpsError('unauthenticated', 'The post asked is a draft' +
+          ' and you do not have the right to get its content.');
+      }
+
+      const decodedToken = await auth.verifyIdToken(jwt, true);
+
+      let hasAuthorAccess = false;
+
+      if (postData['author'] === decodedToken.uid) {
+        hasAuthorAccess = true;
+
+      } else if (postData['coauthors'].indexOf(decodedToken.uid) > -1) {
+        hasAuthorAccess = true;
+      }
+
+      if (!hasAuthorAccess) {
+        throw new functions.https.HttpsError('permission-denied', 'You do not have the right' +
+          " to view this post's content.");
+      }
+    } else if (postData['restrictedTo'].premium) {
+      // TODO: Handle premium users.
+    }
+
+  } catch (error) {
+    throw new functions.https.HttpsError('internal', 'There was an internal error' +
+      ' while retrieving the post content. Your JWT may be outdated.');
+  }
+}
 
 /**
  * Save a post content.
