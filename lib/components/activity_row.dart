e52bc3889a603337e0e73defc12dd2416444ca59
rootasjey <jeremiecorpinot@outlook.com>
2020-09-11 15:54:16 +0200

    👆 Update activities' label & style

--- lib/components/activity_row.dart
+++ lib/components/activity_row.dart
@@ -22,8 +22,7 @@ class _ActivityRowState extends State<ActivityRow> {
 
     return FlatButton.icon(
       onPressed: () {
-        final url = 'https://github.com/${activity.repo.name}';
-        launch(url);
+        launch('https://github.com/${activity.repo.name}');
       },
       icon: Icon(
         getEventIcon(activity.type),
@@ -31,12 +30,25 @@ class _ActivityRowState extends State<ActivityRow> {
       ),
       label: Row(
         children: [
+          Text(
+            '${getEventType(activity.type, activity.payload)}: ',
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+
           Padding(
             padding: const EdgeInsets.only(
               right: 16.0,
             ),
-            child: Text(
-              '${getEventType(activity.type)}: ${activity.repo.name}',
+            child: Opacity(
+              opacity: 0.8,
+              child: Text(
+                '${activity.repo.name}',
+                style: TextStyle(
+                fontWeight: FontWeight.w500,
+              ),
+              ),
             ),
           ),
 
@@ -51,14 +63,52 @@ class _ActivityRowState extends State<ActivityRow> {
     );
   }
 
-  String getEventType(String type) {
+  String getEventType(String type, Map<String, dynamic> payload) {
     switch (type) {
       case 'PushEvent':
         return 'Push';
       case 'WatchEvent':
         return 'Watch';
+      case 'PullRequestEvent':
+        return getPRAction(payload['action']);
+      case 'DeleteEvent':
+        var verb = 'Delete';
+
+        if (payload != null && payload['ref_type'] != null) {
+          verb += ' ${payload['ref_type']}';
+        }
+
+        return verb;
       default:
         return type;
+    }
+  }
+
+  String getPRAction(String action) {
+    final pr = 'PR';
+    switch (action) {
+      case 'opened':
+        return 'Opened $pr';
+      case 'closed':
+        return 'Closed $pr';
+      case 'reopened':
+        return 'Reopened $pr';
+      case 'assigned':
+        return 'Assigned $pr';
+      case 'unassigned':
+        return 'Unassigned $pr';
+      case 'review_requested':
+        return 'Review requested for $pr';
+      case 'review_request_removed':
+        return 'Review request removed for $pr';
+      case 'labeled':
+        return 'Labeled $pr';
+      case 'unlabeled':
+        return 'Unlabeled';
+      case 'synchronize':
+        return 'Synchronize $pr';
+      default:
+        return action;
     }
   }
 
@@ -68,6 +118,10 @@ class _ActivityRowState extends State<ActivityRow> {
         return Icons.publish;
       case 'WatchEvent':
         return Icons.remove_red_eye;
+      case 'PullRequestEvent':
+        return Icons.merge_type;
+      case 'DeleteEvent':
+        return Icons.delete;
       default:
         return Icons.code;
     }
