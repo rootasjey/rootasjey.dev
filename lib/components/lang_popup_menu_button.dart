cedd9929de9c7c78910c94b540f57b89c76c320c
rootasjey <rootasjey@users.noreply.github.com>
2021-03-27 19:03:56 +0100

    ➕ Add LangPopupMenuButton

---
+++ lib/components/lang_popup_menu_button.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:rootasjey/state/colors.dart';
+import 'package:rootasjey/utils/language.dart';
+
+class LangPopupMenuButton extends StatelessWidget {
+  final String lang;
+  final Function(String) onLangChanged;
+  final double opacity;
+  final EdgeInsets margin;
+  final EdgeInsets padding;
+  final double elevation;
+
+  const LangPopupMenuButton({
+    Key key,
+    @required this.lang,
+    @required this.onLangChanged,
+    this.elevation = 0.0,
+    this.opacity = 1.0,
+    this.margin = EdgeInsets.zero,
+    this.padding = const EdgeInsets.all(8.0),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: margin,
+      child: Material(
+        elevation: elevation,
+        borderRadius: BorderRadius.circular(4.0),
+        child: Opacity(
+          opacity: opacity,
+          child: PopupMenuButton<String>(
+            tooltip: "Change language",
+            child: Padding(
+              // padding: const EdgeInsets.all(8.0),
+              padding: padding,
+              child: Text(
+                lang,
+                style: TextStyle(
+                  color: stateColors.foreground,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+            onSelected: onLangChanged,
+            itemBuilder: (context) => Language.available()
+                .map(
+                  (value) => PopupMenuItem(
+                    value: value,
+                    child: Text(value.toUpperCase()),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
