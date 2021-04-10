cdebb971684d886c4ccc5d1291ca805792071d60
rootasjey <rootasjey@users.noreply.github.com>
2021-04-10 18:54:58 +0200

    ☝️ Use [AvatarMenu] on [DashboardPage]

--- lib/screens/dashboard_page.dart
+++ lib/screens/dashboard_page.dart
@@ -1,14 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:rootasjey/components/avatar_menu.dart';
 import 'package:rootasjey/components/side_menu_item.dart';
 import 'package:rootasjey/router/app_router.gr.dart';
 import 'package:rootasjey/state/colors.dart';
 import 'package:rootasjey/utils/constants.dart';
 import 'package:rootasjey/utils/fonts.dart';
 import 'package:unicons/unicons.dart';
-
-import '../components/home_app_bar.dart';
 
 class DashboardPage extends StatefulWidget {
   @override
@@ -82,9 +81,10 @@ class _DashboardPageState extends State<DashboardPage> {
                   top: 20.0,
                   bottom: 50.0,
                 ),
-                sliver: HomeAppBar(
-                  automaticallyImplyLeading: false,
-                  showAppIcon: false,
+                sliver: SliverList(
+                  delegate: SliverChildListDelegate.fixed([
+                    AvatarMenu(),
+                  ]),
                 ),
               ),
               SliverPadding(
