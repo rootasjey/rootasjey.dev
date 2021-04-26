b9b6fbfa5ab9fc6e5c541ff169db20bfc8479829
rootasjey <rootasjey@users.noreply.github.com>
2021-04-26 14:23:15 +0200

    ➕ Add [convertFromFun] to convert data from a Cloud Fun

--- lib/utils/cloud.dart
+++ lib/utils/cloud.dart
@@ -1,3 +1,6 @@
+import 'dart:collection';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -7,5 +10,25 @@ class Cloud {
       app: Firebase.app(),
       region: 'europe-west3',
     ).httpsCallable(name);
+  }
+
+  static Map<String, dynamic> convertFromFun(
+      LinkedHashMap<dynamic, dynamic> raw) {
+    final hashMap = LinkedHashMap.from(raw);
+
+    final Map<String, dynamic> converted = hashMap.map((key, value) {
+      if (value is String ||
+          value is num ||
+          value is bool ||
+          value is Timestamp ||
+          value == null) {
+        return MapEntry(key, value);
+      }
+
+      final d2 = convertFromFun(value);
+      return MapEntry(key, d2);
+    });
+
+    return converted;
   }
 }
