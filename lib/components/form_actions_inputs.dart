41302ee1a6d76a85b54019f9babe8d85f96a4a68
rootasjey <rootasjey@users.noreply.github.com>
2021-05-04 02:55:17 +0200

    ➕ Add [FormActionInputs] class

---
+++ lib/components/form_actions_inputs.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:rootasjey/state/colors.dart';
+import 'package:rootasjey/utils/constants.dart';
+import 'package:unicons/unicons.dart';
+
+/// Use as a form footer to cancel of validate data.
+class FormActionInputs extends StatelessWidget {
+  /// Executed when the form is closed with the cancel button.
+  final VoidCallback onCancel;
+
+  /// Executed when the form is closed with validate button.
+  final VoidCallback onValidate;
+
+  /// Cancel button's text string value.
+  final String cancelTextString;
+
+  /// Save button's text string value.
+  final String saveTextString;
+
+  /// Padding around the buttons.
+  final EdgeInsets padding;
+
+  /// Automatically adjust padding according to app's widget.
+  final bool adaptivePadding;
+
+  const FormActionInputs({
+    Key key,
+    this.onCancel,
+    this.onValidate,
+    this.cancelTextString = 'Clear input',
+    this.saveTextString = 'Save',
+    this.padding = const EdgeInsets.only(
+      top: 20.0,
+    ),
+    this.adaptivePadding = true,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double left = 40.0;
+    double spacing = 24.0;
+
+    EdgeInsets _padding = padding;
+    final isSmall =
+        MediaQuery.of(context).size.width < Constants.maxMobileWidth;
+
+    if (adaptivePadding && isSmall) {
+      spacing = 12.0;
+      left = 0.0;
+
+      _padding = EdgeInsets.only(
+        top: 20.0,
+        left: left,
+      );
+    }
+
+    return Padding(
+      padding: _padding,
+      child: Wrap(
+        alignment: WrapAlignment.end,
+        spacing: spacing,
+        runSpacing: 10.0,
+        children: [
+          Opacity(
+            opacity: 0.6,
+            child: OutlinedButton.icon(
+              onPressed: onCancel,
+              icon: Icon(UniconsLine.times),
+              label: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 12.0,
+                  horizontal: 12.0,
+                ),
+                child: Text(
+                  cancelTextString,
+                ),
+              ),
+              style: OutlinedButton.styleFrom(
+                primary: stateColors.foreground,
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {
+              if (onValidate != null) {
+                onValidate();
+              }
+
+              Navigator.of(context).pop();
+            },
+            icon: Icon(UniconsLine.check),
+            label: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 12.0,
+                horizontal: 12.0,
+              ),
+              child: Text(saveTextString),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: stateColors.validation,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
