3bc393dc0680b1c7576b3d1191f7061a33b0cdd3
rootasjey <rootasjey@users.noreply.github.com>
2021-05-23 10:42:49 +0200

    feat(component): :sparkles: add mobile view to LandingPosts

--- lib/components/landing_posts.dart
+++ lib/components/landing_posts.dart
@@ -2,11 +2,13 @@ import 'package:auto_route/auto_route.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:rootasjey/components/arrow_divider.dart';
+import 'package:rootasjey/components/min_pub_post_card.dart';
 import 'package:rootasjey/components/new_pub_post_card.dart';
 import 'package:rootasjey/router/app_router.gr.dart';
 import 'package:rootasjey/state/colors.dart';
 import 'package:rootasjey/types/post_headline.dart';
 import 'package:rootasjey/utils/app_logger.dart';
+import 'package:rootasjey/utils/constants.dart';
 import 'package:rootasjey/utils/fonts.dart';
 import 'package:unicons/unicons.dart';
 
@@ -16,6 +18,7 @@ class LandingPosts extends StatefulWidget {
 }
 
 class _LandingPostsState extends State<LandingPosts> {
+  bool _isSmallView = false;
   List<PostHeadline> posts = [];
 
   @override
@@ -26,6 +29,26 @@ class _LandingPostsState extends State<LandingPosts> {
 
   @override
   Widget build(BuildContext context) {
+    _isSmallView = false;
+
+    final viewWidth = MediaQuery.of(context).size.width;
+
+    EdgeInsets padding = const EdgeInsets.only(
+      top: 100.0,
+      left: 120.0,
+      right: 120.0,
+    );
+
+    if (viewWidth < Constants.maxMobileWidth) {
+      _isSmallView = true;
+
+      padding = const EdgeInsets.only(
+        top: 80.0,
+        left: 20.0,
+        right: 20.0,
+      );
+    }
+
     return Container(
       color: stateColors.newLightBackground,
       child: Column(
@@ -33,11 +56,7 @@ class _LandingPostsState extends State<LandingPosts> {
         children: [
           ArrowDivider(),
           Padding(
-            padding: const EdgeInsets.only(
-              top: 100.0,
-              left: 120.0,
-              right: 120.0,
-            ),
+            padding: padding,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -54,17 +73,20 @@ class _LandingPostsState extends State<LandingPosts> {
   }
 
   Widget title() {
-    return Text(
-      "Latest Posts",
-      style: FontsUtils.mainStyle(
-        fontSize: 100.0,
-        height: 1.2,
-        fontWeight: FontWeight.w800,
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0),
+      child: Text(
+        "Latest Posts",
+        style: FontsUtils.mainStyle(
+          fontSize: _isSmallView ? 60.0 : 100.0,
+          height: 1.2,
+          fontWeight: FontWeight.w800,
+        ),
       ),
     );
   }
 
-  Widget postsWrap() {
+  Widget postsDesktopView() {
     return Padding(
       padding: const EdgeInsets.only(
         top: 16.0,
@@ -79,6 +101,32 @@ class _LandingPostsState extends State<LandingPosts> {
         }).toList(),
       ),
     );
+  }
+
+  Widget postsMobileView() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 24.0),
+      child: Column(
+        children: posts.map((post) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              MinPubPostCard(postHeadline: post),
+              Divider(height: 40.0),
+            ],
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget postsWrap() {
+    if (_isSmallView) {
+      return postsMobileView();
+    }
+
+    return postsDesktopView();
   }
 
   Widget viewMoreButton() {
