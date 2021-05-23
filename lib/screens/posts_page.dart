a5746f2a30b5115e4c65f76d8bfc573363b0eb77
rootasjey <rootasjey@users.noreply.github.com>
2021-05-23 23:00:49 +0200

    feat(page): :sparkles: update PostsPage w/ better cards, fetch more
    Update PostsPage with better post cards, ability to fetch more, better mobile view.

--- lib/screens/posts_page.dart
+++ lib/screens/posts_page.dart
@@ -1,14 +1,15 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:rootasjey/components/better_post_card.dart';
+import 'package:rootasjey/components/main_app_bar.dart';
+import 'package:rootasjey/components/min_pub_post_card.dart';
 import 'package:rootasjey/components/page_title.dart';
-import 'package:rootasjey/components/post_card.dart';
 import 'package:rootasjey/components/sliver_empty_view.dart';
-import 'package:rootasjey/components/home_app_bar.dart';
-import 'package:rootasjey/router/app_router.gr.dart';
+import 'package:rootasjey/state/colors.dart';
 import 'package:rootasjey/types/post.dart';
 import 'package:rootasjey/utils/app_logger.dart';
+import 'package:rootasjey/utils/constants.dart';
 
 class PostsPage extends StatefulWidget {
   @override
@@ -16,16 +17,15 @@ class PostsPage extends StatefulWidget {
 }
 
 class _PostsPageState extends State<PostsPage> {
-  final postsList = <Post>[];
+  bool _isSmallView = false;
+  bool _hasNext = true;
+  bool _isLoading = false;
 
-  final largeHorizPadding = 90.0;
-  final narrowHorizPadding = 20.0;
-  final narrowWidthLimit = 800.0;
+  DocumentSnapshot _lastDoc;
 
-  final limit = 10;
-  bool hasNext = true;
-  bool isLoading = false;
-  var lastDoc;
+  final _posts = <Post>[];
+
+  final int _limit = 10;
 
   @override
   void initState() {
@@ -35,114 +35,186 @@ class _PostsPageState extends State<PostsPage> {
 
   @override
   Widget build(BuildContext context) {
+    final viewWidth = MediaQuery.of(context).size.width;
+    _isSmallView = viewWidth < Constants.maxMobileWidth;
+
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          HomeAppBar(),
-          SliverLayoutBuilder(
-            builder: (_, constraints) {
-              final padding = constraints.crossAxisExtent < narrowWidthLimit
-                  ? narrowHorizPadding
-                  : largeHorizPadding;
-
-              return SliverList(
-                delegate: SliverChildListDelegate([
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: padding,
-                      vertical: 90.0,
-                    ),
-                    child: PageTitle(
-                      textTitle: "posts".tr(),
-                      isLoading: isLoading,
-                    ),
-                  ),
-                ]),
-              );
-            },
-          ),
-          SliverLayoutBuilder(
-            builder: (_, constraints) {
-              final padding = constraints.crossAxisExtent < narrowWidthLimit
-                  ? narrowHorizPadding
-                  : largeHorizPadding;
-
-              return SliverPadding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: padding,
-                ),
-                sliver: body(),
-              );
-            },
-          ),
-        ],
+      backgroundColor: stateColors.newLightBackground,
+      body: NotificationListener<ScrollNotification>(
+        onNotification: onNotification,
+        child: CustomScrollView(
+          slivers: [
+            MainAppBar(),
+            title(),
+            SliverPadding(
+              padding: EdgeInsets.only(
+                left: _isSmallView ? 20.0 : 80.0,
+                right: 20.0,
+                bottom: 400.0,
+              ),
+              sliver: body(),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   Widget body() {
-    if (!isLoading && postsList.isEmpty) {
+    if (!_isLoading && _posts.isEmpty) {
       return SliverEmptyView();
     }
 
-    return postsListView();
+    if (_isSmallView) {
+      return postsListView();
+    }
+
+    return postsGridView();
+  }
+
+  Widget postsGridView() {
+    return SliverGrid(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 360.0,
+        childAspectRatio: 0.6,
+      ),
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          final post = _posts.elementAt(index);
+          return BetterPostCard(post: post);
+        },
+        childCount: _posts.length,
+      ),
+    );
   }
 
   Widget postsListView() {
     return SliverList(
       delegate: SliverChildBuilderDelegate(
         (context, index) {
-          final post = postsList.elementAt(index);
+          final post = _posts.elementAt(index);
 
-          return PostCard(
-            onTap: () {
-              context.router.push(PostPageRoute(postId: post.id));
-            },
-            post: post,
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              MinPubPostCard(post: post),
+              Divider(height: 40.0),
+            ],
           );
         },
-        childCount: postsList.length,
+        childCount: _posts.length,
       ),
+    );
+  }
+
+  Widget title() {
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        Padding(
+          padding: EdgeInsets.only(
+            left: _isSmallView ? 40.0 : 100.0,
+            top: 90.0,
+            bottom: 40.0,
+          ),
+          child: PageTitle(
+            textTitle: "posts".tr(),
+            isLoading: _isLoading,
+          ),
+        ),
+      ]),
     );
   }
 
   void fetch() async {
     setState(() {
-      postsList.clear();
-      isLoading = true;
+      _posts.clear();
+      _isLoading = true;
     });
 
     try {
       final snapshot = await FirebaseFirestore.instance
           .collection('posts')
           .where('published', isEqualTo: true)
-          .limit(limit)
+          .limit(_limit)
           .get();
 
       if (snapshot.size == 0) {
         setState(() {
-          hasNext = false;
-          isLoading = false;
+          _hasNext = false;
+          _isLoading = false;
         });
 
         return;
       }
 
-      lastDoc = snapshot.docs.last;
+      snapshot.docs.forEach((doc) {
+        final data = doc.data();
+        data['id'] = doc.id;
+
+        _posts.add(Post.fromJSON(data));
+      });
+
+      setState(() {
+        _isLoading = false;
+        _lastDoc = snapshot.docs.last;
+        _hasNext = _limit == snapshot.size;
+      });
+    } catch (error) {
+      appLogger.e(error);
+    }
+  }
+
+  void fetchMore() async {
+    if (!_hasNext || _isLoading || _lastDoc == null) {
+      return;
+    }
+
+    setState(() => _isLoading = true);
+
+    try {
+      final snapshot = await FirebaseFirestore.instance
+          .collection('posts')
+          .where('published', isEqualTo: true)
+          .startAfterDocument(_lastDoc)
+          .limit(_limit)
+          .get();
+
+      if (snapshot.size == 0) {
+        setState(() {
+          _hasNext = false;
+          _isLoading = false;
+        });
+
+        return;
+      }
 
       snapshot.docs.forEach((doc) {
         final data = doc.data();
         data['id'] = doc.id;
 
-        postsList.add(Post.fromJSON(data));
+        _posts.add(Post.fromJSON(data));
       });
 
       setState(() {
-        isLoading = false;
-        hasNext = limit == snapshot.size;
+        _isLoading = false;
+        _lastDoc = snapshot.docs.last;
+        _hasNext = _limit == snapshot.size;
       });
     } catch (error) {
       appLogger.e(error);
     }
+  }
+
+  bool onNotification(ScrollNotification notification) {
+    final double current = notification.metrics.pixels;
+    final double max = notification.metrics.maxScrollExtent;
+
+    if (current < max - 300.0) {
+      return false;
+    }
+
+    fetchMore();
+    return false;
   }
 }
