fc0993d563eca6957b7f74b15821edf2a479a1eb
rootasjey <rootasjey@users.noreply.github.com>
2021-05-25 00:22:39 +0200

    feat(page): :sparkles: replace SimpleAppBar → MainAppBar
    Replace SimpleAppBar → MainAppBar on MyPostsPage & MyProjectsPage

--- lib/screens/my_projects_page.dart
+++ lib/screens/my_projects_page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:rootasjey/components/simple_app_bar.dart';
+import 'package:rootasjey/components/main_app_bar.dart';
 import 'package:rootasjey/router/app_router.gr.dart';
 
 class MyProjectsPage extends StatefulWidget {
@@ -18,8 +17,8 @@ class _MyProjectsPageState extends State<MyProjectsPage> {
         PublishedProjectsPageRoute(),
       ],
       appBarBuilder: (context, tabsRouter) {
-        return SimpleAppBar(
-          textTitle: "posts".tr(),
+        return MainAppBar(
+          renderSliver: false,
         );
       },
       builder: (context, child, animation) {
