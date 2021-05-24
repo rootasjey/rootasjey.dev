5fec20d6c984742e6c1f10bad7eb7ba585ed0c39
rootasjey <rootasjey@users.noreply.github.com>
2021-05-24 23:53:17 +0200

    feat(page): :sparkles: update ui, add fetchMore()

--- lib/screens/projects_page.dart
+++ lib/screens/projects_page.dart
@@ -2,13 +2,15 @@ import 'package:auto_route/auto_route.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:rootasjey/components/footer.dart';
+import 'package:rootasjey/components/main_app_bar.dart';
 import 'package:rootasjey/components/page_title.dart';
 import 'package:rootasjey/components/project_card.dart';
 import 'package:rootasjey/components/sliver_empty_view.dart';
-import 'package:rootasjey/components/home_app_bar.dart';
 import 'package:rootasjey/router/app_router.gr.dart';
 import 'package:rootasjey/types/project.dart';
 import 'package:rootasjey/utils/app_logger.dart';
+import 'package:rootasjey/utils/constants.dart';
 
 class ProjectsPage extends StatefulWidget {
   @override
@@ -16,16 +18,15 @@ class ProjectsPage extends StatefulWidget {
 }
 
 class _ProjectsPageState extends State<ProjectsPage> {
-  final projectsList = <Project>[];
+  final _projects = <Project>[];
 
-  final largeHorizPadding = 90.0;
-  final narrowHorizPadding = 20.0;
-  final narrowWidthLimit = 800.0;
+  final _largeHorizPadding = 90.0;
+  final _narrowHorizPadding = 20.0;
 
-  final limit = 10;
-  bool hasNext = true;
-  bool isLoading = false;
-  var lastDoc;
+  final _limit = 10;
+  bool _hasNext = true;
+  bool _isLoading = false;
+  DocumentSnapshot _lastDoc;
 
   @override
   void initState() {
@@ -36,57 +37,70 @@ class _ProjectsPageState extends State<ProjectsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          HomeAppBar(),
-          SliverLayoutBuilder(
-            builder: (_, constraints) {
-              final padding = constraints.crossAxisExtent < narrowWidthLimit
-                  ? narrowHorizPadding
-                  : largeHorizPadding;
-
-              return SliverPadding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: padding,
-                  vertical: 90.0,
-                ),
-                sliver: SliverList(
-                  delegate: SliverChildListDelegate([
-                    PageTitle(
-                      textTitle: "projects".tr(),
-                      isLoading: isLoading,
-                    ),
-                  ]),
-                ),
-              );
-            },
-          ),
-          SliverLayoutBuilder(
-            builder: (_, constraints) {
-              final padding = constraints.crossAxisExtent < narrowWidthLimit
-                  ? narrowHorizPadding
-                  : largeHorizPadding;
-
-              return SliverPadding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: padding,
-                  vertical: 90.0,
-                ),
-                sliver: body(),
-              );
-            },
-          ),
-        ],
+      body: NotificationListener<ScrollNotification>(
+        onNotification: onNotification,
+        child: CustomScrollView(
+          slivers: [
+            MainAppBar(),
+            header(),
+            body(),
+            footer(),
+          ],
+        ),
       ),
     );
   }
 
   Widget body() {
-    if (!isLoading && projectsList.isEmpty) {
-      return SliverEmptyView();
+    Widget child;
+
+    if (_isLoading || _projects.isEmpty) {
+      child = SliverEmptyView();
+    } else {
+      child = projectsGrid();
     }
 
-    return projectsGrid();
+    final padding = MediaQuery.of(context).size.width < Constants.maxMobileWidth
+        ? _narrowHorizPadding
+        : _largeHorizPadding;
+
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(
+        horizontal: padding,
+        vertical: 90.0,
+      ),
+      sliver: child,
+    );
+  }
+
+  Widget footer() {
+    return SliverList(
+      delegate: SliverChildListDelegate.fixed([
+        Footer(),
+      ]),
+    );
+  }
+
+  Widget header() {
+    final padding = MediaQuery.of(context).size.width < Constants.maxMobileWidth
+        ? _narrowHorizPadding
+        : _largeHorizPadding;
+
+    return SliverPadding(
+      padding: EdgeInsets.only(
+        left: padding,
+        right: padding,
+        top: 90.0,
+      ),
+      sliver: SliverList(
+        delegate: SliverChildListDelegate([
+          PageTitle(
+            textTitle: "projects".tr(),
+            isLoading: _isLoading,
+          ),
+        ]),
+      ),
+    );
   }
 
   Widget projectsGrid() {
@@ -98,7 +112,7 @@ class _ProjectsPageState extends State<ProjectsPage> {
       ),
       delegate: SliverChildBuilderDelegate(
         (context, index) {
-          final project = projectsList.elementAt(index);
+          final project = _projects.elementAt(index);
 
           return ProjectCard(
             project: project,
@@ -109,48 +123,100 @@ class _ProjectsPageState extends State<ProjectsPage> {
             },
           );
         },
-        childCount: projectsList.length,
+        childCount: _projects.length,
       ),
     );
   }
 
   void fetch() async {
     setState(() {
-      projectsList.clear();
-      isLoading = true;
+      _projects.clear();
+      _isLoading = true;
     });
 
     try {
       final snapshot = await FirebaseFirestore.instance
           .collection('projects')
           .where('published', isEqualTo: true)
-          .limit(limit)
+          .limit(_limit)
           .get();
 
       if (snapshot.size == 0) {
         setState(() {
-          hasNext = false;
-          isLoading = false;
+          _hasNext = false;
+          _isLoading = false;
         });
 
         return;
       }
 
-      lastDoc = snapshot.docs.last;
+      snapshot.docs.forEach((doc) {
+        final data = doc.data();
+        data['id'] = doc.id;
+
+        _projects.add(Project.fromJSON(data));
+      });
+
+      setState(() {
+        _isLoading = false;
+        _lastDoc = snapshot.docs.last;
+        _hasNext = _limit == snapshot.size;
+      });
+    } catch (error) {
+      appLogger.e(error);
+    }
+  }
+
+  void fetchMore() async {
+    if (!_hasNext || _isLoading || _lastDoc == null) {
+      return;
+    }
+
+    setState(() => _isLoading = true);
+
+    try {
+      final snapshot = await FirebaseFirestore.instance
+          .collection('projects')
+          .where('published', isEqualTo: true)
+          .startAfterDocument(_lastDoc)
+          .limit(_limit)
+          .get();
+
+      if (snapshot.size == 0) {
+        setState(() {
+          _hasNext = false;
+          _isLoading = false;
+        });
+
+        return;
+      }
 
       snapshot.docs.forEach((doc) {
         final data = doc.data();
         data['id'] = doc.id;
 
-        projectsList.add(Project.fromJSON(data));
+        _projects.add(Project.fromJSON(data));
       });
 
       setState(() {
-        isLoading = false;
-        hasNext = limit == snapshot.size;
+        _isLoading = false;
+        _lastDoc = snapshot.docs.last;
+        _hasNext = _limit == snapshot.size;
       });
     } catch (error) {
       appLogger.e(error);
     }
+  }
+
+  bool onNotification(ScrollNotification notification) {
+    final double current = notification.metrics.pixels;
+    final double max = notification.metrics.maxScrollExtent;
+
+    if (current < max - 300.0) {
+      return false;
+    }
+
+    fetchMore();
+    return false;
   }
 }
