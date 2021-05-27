e04d2905f43992d24880ffd8659490ce79b737a5
rootasjey <rootasjey@users.noreply.github.com>
2021-05-28 00:32:15 +0200

    feat(component): :lipstick: update font on SheetHeader

--- lib/components/sheet_header.dart
+++ lib/components/sheet_header.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:rootasjey/components/circle_button.dart';
 import 'package:rootasjey/state/colors.dart';
+import 'package:rootasjey/utils/fonts.dart';
 import 'package:unicons/unicons.dart';
 
 class SheetHeader extends StatelessWidget {
@@ -20,39 +21,36 @@ class SheetHeader extends StatelessWidget {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        CircleButton(
-          onTap: context.router.pop,
-          tooltip: tooltip,
-          icon: Icon(
-            UniconsLine.times,
-            size: 20.0,
-            color: stateColors.primary,
-          ),
-        ),
         Padding(
-          padding: const EdgeInsets.only(left: 10.0),
+          padding: const EdgeInsets.only(right: 16.0),
+          child: CircleButton(
+            onTap: context.router.pop,
+            tooltip: tooltip,
+            icon: Icon(
+              UniconsLine.times,
+              size: 20.0,
+              color: stateColors.primary,
+            ),
+          ),
         ),
         Expanded(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 5.0),
-                child: Opacity(
-                  opacity: 0.6,
-                  child: Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w500,
-                    ),
+              Opacity(
+                opacity: 0.4,
+                child: Text(
+                  title,
+                  style: FontsUtils.mainStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
               if (subtitle != null)
                 Text(
                   subtitle,
-                  style: TextStyle(
+                  style: FontsUtils.mainStyle(
                     fontSize: 18.0,
                     fontWeight: FontWeight.w500,
                   ),
