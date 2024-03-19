2f56c57012213762ecfb771d1dd6f610f36b7cc9
rootasjey <5097282+rootasjey@users.noreply.github.com>
2024-03-19 20:30:29 +0100

    feat: :sparkles: add `useSliver` arg to `LoadingView`

--- lib/components/loading_view.dart
+++ lib/components/loading_view.dart
@@ -7,37 +7,47 @@ class LoadingView extends StatelessWidget {
   const LoadingView({
     super.key,
     this.message = "loading...",
+    this.useSliver = false,
   });
+
+  /// Whether to use a [SliverToBoxAdapter] or not.
+  final bool useSliver;
 
   /// Message value to display as a loading message.
   final String message;
 
   @override
   Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: Lottie.asset(
-                "assets/animations/box.json",
-                width: 300.0,
+    final Center center = Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            child: Lottie.asset(
+              "assets/animations/box.json",
+              width: 300.0,
+            ),
+          ),
+          Text(
+            message,
+            style: Utils.calligraphy.body2(
+              textStyle: const TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.w600,
               ),
             ),
-            Text(
-              message,
-              style: Utils.calligraphy.body2(
-                textStyle: const TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+
+    if (useSliver) {
+      return SliverToBoxAdapter(
+        child: center,
+      );
+    }
+
+    return center;
   }
 
   /// Return a Scaffold widget displaying a loading animation.
